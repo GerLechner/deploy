cccef38e20981608342b1c10ad7240171aac58a3
German Agustin Lechner <glechner@frba.utn.edu.ar>
2023-11-20 14:10:19 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-#server.py
+
 from flask import Flask, render_template
+from logging import FileHandler, WARNING
 import sys
 import os
 
-app = Flask(__name__, template_folder="/")
+app = Flask(__name__, template_folder = 'templates')
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/romper')
 def romper():
@@ -16,4 +19,3 @@ def romper():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-#-------------------------------------
