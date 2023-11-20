9a131388ea53bb23edae428a616e81c4d67ddea4
German Agustin Lechner <glechner@frba.utn.edu.ar>
2023-11-20 11:02:29 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('src/index.html')
+    return render_template('/src/index.html')
 
 @app.route('/romper')
 def romper():
